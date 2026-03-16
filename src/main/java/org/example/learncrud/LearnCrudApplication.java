83754cea7d9ba18b44d27be14ef2a4c177f2001c
hikeong <s25048@gsm.hs.kr>
2026-03-16 18:26:33 +0900

    init

---
+++ src/main/java/org/example/learncrud/LearnCrudApplication.java
@@ -0,0 +1,13 @@
+package org.example.learncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCrudApplication.class, args);
+	}
+
+}
